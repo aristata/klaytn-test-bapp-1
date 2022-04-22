3558e7366522ab4541dd1290c06a3a2f0fe54388
장성민 <earnestgem@optatumplatform.com>
2022-04-22 10:48:16 +0900

    완료

--- src/index.js
+++ src/index.js
@@ -24,6 +24,37 @@ const App = {
     },
     
     start: async function () {
+        // const tokenAddress = '0xcac25b35f3708acac59c505228597b0b664bc640'
+        // const tokenSymbol = 'SBPT'
+        // const tokenDecimals = 8
+        // const tokenImage = 'https://www.playgrounds.co.kr/images/logo.png'
+    
+        // klaytn.sendAsync(
+        //     {
+        //         method: 'wallet_watchAsset',
+        //         params: {
+        //             type: 'ERC20', // Initially only supports ERC20, but eventually more!
+        //             options: {
+        //                 address: tokenAddress, // The address that the token is at.
+        //                 symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+        //                 decimals: tokenDecimals, // The number of decimals in the token
+        //                 image: tokenImage // A string url of the token logo
+        //             }
+        //         },
+        //         id: Math.round(Math.random() * 100000)
+        //     },
+        //     (err, added) => {
+        //         if (added) {
+        //             console.log('Thanks for your interest!')
+        //         } else {
+        //             console.log('Your loss!')
+        //         }
+        //     }
+        // )
+    
+        const sender = caver.klay.accounts.wallet[0];
+        console.log(sender)
+        
         // 세션 스토리지에서 월렛 인스턴스를 가져온다
         const walletFromSession = sessionStorage.getItem('walletInstance')
         
@@ -77,13 +108,15 @@ const App = {
             try {
                 // keystore 파일과 비밀번호로 비밀키를 얻는다
                 const privateKey = cav.klay.accounts.decrypt(this.auth.keystore, this.auth.password).privateKey
-                
+
                 // 지갑 연동 함수를 호출
                 this.integrateWallet(privateKey)
             } catch (e) {
                 $('#message').text('비밀번호가 일치하지 않습니다.')
             }
         }
+    
+        
     },
     
     handleLogout: async function () {
